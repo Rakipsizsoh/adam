95aae1402dc02c5667331db960f0c6f503481e71
Rakipsizsoh <139055178+Rakipsizsoh@users.noreply.github.com>
2026-05-03 17:06:12 +0300

    Add files via upload

---
+++ MyIRC.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,18 @@
+﻿using BCrypt.Net;
+using MyIRC.Application.Interfaces.Security;
+
+namespace MyIRC.Infrastructure.Security
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
