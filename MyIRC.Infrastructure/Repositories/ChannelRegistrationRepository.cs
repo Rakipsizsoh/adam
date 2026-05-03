3f194a885fe4eefaa7eee0f954a7462182714c05
Rakipsizsoh <139055178+Rakipsizsoh@users.noreply.github.com>
2026-05-03 17:05:57 +0300

    Add files via upload

---
+++ MyIRC.Infrastructure/Repositories/ChannelRegistrationRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyIRC.Application.Interfaces.Repositories;
+using MyIRC.Domain.Entities.Channels;
+using MyIRC.Infrastructure.Data;
+
+namespace MyIRC.Infrastructure.Repositories
+{
+    public class ChannelRegistrationRepository : IChannelRegistrationRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ChannelRegistrationRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> ExistsByChannelAsync(string channelName)
+        {
+            return await _context.ChannelRegistrations
+                .AnyAsync(x => x.ChannelName == channelName && x.IsActive);
+        }
+
+        public async Task<ChannelRegistration?> GetByChannelAsync(string channelName)
+        {
+            return await _context.ChannelRegistrations
+                .FirstOrDefaultAsync(x => x.ChannelName == channelName && x.IsActive);
+        }
+
+        public async Task AddAsync(ChannelRegistration registration)
+        {
+            _context.ChannelRegistrations.Add(registration);
+            await _context.SaveChangesAsync();
+        }
+
+        // 🔥 MODE için gerekli UPDATE
+        public async Task UpdateAsync(ChannelRegistration registration)
+        {
+            _context.ChannelRegistrations.Update(registration);
+            await _context.SaveChangesAsync();
+        }
+
+        // 🔥 AUTO JOIN
+
+        public async Task<List<string>> GetDefaultJoinChannelsAsync()
+        {
+            return await _context.ChannelRegistrations
+                .Where(x => x.IsActive && x.IsDefaultJoin)
+                .OrderBy(x => x.Id)
+                .Select(x => x.ChannelName)
+                .ToListAsync();
+        }
+    }
+}
---
+++ MyIRC.Infrastructure/Repositories/UserAccountRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyIRC.Application.Interfaces.Repositories;
+using MyIRC.Domain.Entities.Users;
+using MyIRC.Infrastructure.Data;
+
+namespace MyIRC.Infrastructure.Repositories
+{
+    public class UserAccountRepository : IUserAccountRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserAccountRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> ExistsByNickAsync(string nick)
+        {
+            return await _context.Users
+                .AnyAsync(x => x.Nick.ToLower() == nick.ToLower());
+        }
+
+        public async Task<UserAccount?> GetByNickAsync(string nick)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(x => x.Nick.ToLower() == nick.ToLower());
+        }
+
+        public async Task AddAsync(UserAccount user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
